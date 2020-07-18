09e158fe2c5325f76a613ea904b6a9ae2c5b9110
brahim-derouiche <derouichebrahim66@gmail.com>
2020-07-18 09:30:05 +0100

    done

---
+++ main.js
@@ -0,0 +1,198 @@
+// $("#img").hide()
+
+// $(document).ready(function(){
+// 	$("#Coffee").click(function(){
+		
+// 	})	
+// });
+
+
+		$("#img2").hide()
+		$("#img3").hide()
+		$("#img").hide()
+
+
+// $("#img2").hide()
+
+$(document).ready(function(){
+	$("#Coffee").click(function(){
+		$("#img2").fadeToggle()
+		$("#img3").fadeToggle()
+		$("#img").fadeToggle()
+
+		$("#img4").hide()
+		$("#img5").hide()
+		$("#img6").hide()
+
+        $("#img7").hide()
+		$("#img8").hide()
+		$("#img9").hide()
+
+        $("#img10").hide()
+		$("#img11").hide()
+		$("#img12").hide()	
+		
+        $("#img13").hide()
+		$("#img14").hide()
+		$("#img15").hide()
+		$("#t1").hide()
+		$("#t2").hide()
+		$("#t3").hide()			
+
+
+	})	
+});
+
+
+// $("#img3").hide()
+
+// $(document).ready(function(){
+// 	$("#Coffee").click(function(){
+		
+// 	})	
+// });
+		
+		$("#img4").hide()
+		$("#img5").hide()
+		$("#img6").hide()
+
+
+
+	$("#cats").click(function(){
+		
+		$("#img4").fadeToggle()
+		$("#img5").fadeToggle()
+		$("#img6").fadeToggle()
+
+		$("#img2").hide()
+		$("#img3").hide()
+		$("#img").hide()
+
+        $("#img7").hide()
+		$("#img8").hide()
+		$("#img9").hide()
+
+        $("#img10").hide()
+		$("#img11").hide()
+		$("#img12").hide()				
+
+        $("#img13").hide()
+		$("#img14").hide()
+		$("#img15").hide()
+		$("#t1").hide()
+		$("#t2").hide()
+		$("#t3").hide()
+	})	
+
+
+        $("#img7").hide()
+		$("#img8").hide()
+		$("#img9").hide()
+
+
+
+	$("#nature").click(function(){
+		
+		$("#img7").fadeToggle()
+		$("#img8").fadeToggle()
+		$("#img9").fadeToggle()
+
+        $("#img10").hide()
+		$("#img11").hide()
+		$("#img12").hide()				
+
+        $("#img13").hide()
+		$("#img14").hide()
+		$("#img15").hide()
+		$("#t1").hide()
+		$("#t2").hide()
+		$("#t3").hide()	
+		
+		$("#img2").hide()	
+		$("#img3").hide()	
+		$("#img").hide()	
+
+		$("#img4").hide()
+		$("#img5").hide()
+		$("#img6").hide()
+
+
+        $("#img10").hide()
+		$("#img11").hide()
+		$("#img12").hide()					
+	})	
+
+
+        $("#img10").hide()
+		$("#img11").hide()
+		$("#img12").hide()
+
+
+
+	$("#random").click(function(){
+		
+		$("#img10").fadeToggle()
+		$("#img11").fadeToggle()
+		$("#img12").fadeToggle()
+
+		$("#img7").hide()
+		$("#img8").hide()
+		$("#img9").hide()
+
+		$("#img4").hide()
+		$("#img5").hide()
+		$("#img6").hide()
+
+		$("#img2").hide()
+		$("#img3").hide()
+		$("#img").hide()
+
+        $("#img13").hide()
+		$("#img14").hide()
+		$("#img15").hide()
+		$("#t1").hide()
+		$("#t2").hide()
+		$("#t3").hide()				
+
+	})	
+
+
+        $("#img13").hide()
+		$("#img14").hide()
+		$("#img15").hide()
+		$("#t1").hide()
+		$("#t2").hide()
+		$("#t3").hide()
+
+
+	$("#fp").click(function(){
+		
+		$("#img13").fadeToggle()
+		$("#img14").fadeToggle()
+		$("#img15").fadeToggle()
+		$("#t1").fadeToggle()
+		$("#t2").fadeToggle()
+		$("#t3").fadeToggle()
+
+		$("#img7").hide()
+		$("#img8").hide()
+		$("#img9").hide()
+
+		$("#img4").hide()
+		$("#img5").hide()
+		$("#img6").hide()
+
+		$("#img2").hide()
+		$("#img3").hide()
+		$("#img").hide()
+
+        $("#img10").hide()
+		$("#img11").hide()
+		$("#img12").hide()
+	});
+	
+
+
+
+
+
